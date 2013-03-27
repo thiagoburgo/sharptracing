251029eae4586060776ce95cf9bf881603a4041b
thiagoburgo@gmail.com <thiagoburgo@gmail.com@7d6479aa-596b-11de-bc6d-35d2ebd5fd4e>
2013-03-27 06:49:39 +0000

    Weifenluo update API

--- ShapTracingUI/ToolBoxWindow.Designer.cs
+++ ShapTracingUI/ToolBoxWindow.Designer.cs
@@ -31,18 +31,19 @@ namespace DrawEngine.SharpTracingUI {
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(253, 462);
-            this.DockAreas = ((WeifenLuo.WinFormsUI.Docking.DockAreas)(((((WeifenLuo.WinFormsUI.Docking.DockAreas.Float | WeifenLuo.WinFormsUI.Docking.DockAreas.DockLeft)
-                        | WeifenLuo.WinFormsUI.Docking.DockAreas.DockRight)
-                        | WeifenLuo.WinFormsUI.Docking.DockAreas.DockTop)
-                        | WeifenLuo.WinFormsUI.Docking.DockAreas.DockBottom)));
+            this.DockAreas = ((WeifenLuo.WinFormsUI.Docking.DockAreas)(((((WeifenLuo.WinFormsUI.Docking.DockAreas.Float | WeifenLuo.WinFormsUI.Docking.DockAreas.DockLeft) 
+            | WeifenLuo.WinFormsUI.Docking.DockAreas.DockRight) 
+            | WeifenLuo.WinFormsUI.Docking.DockAreas.DockTop) 
+            | WeifenLuo.WinFormsUI.Docking.DockAreas.DockBottom)));
             this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.HideOnClose = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ToolBoxWindow";
             this.ShowHint = WeifenLuo.WinFormsUI.Docking.DockState.DockLeft;
             this.ShowInTaskbar = false;
-            this.TabText = "ToolBoxWindow";
-            this.Text = "ToolBoxWindow";
+            this.TabText = "Objects";
+            this.Text = "Objects";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ToolBoxWindow_FormClosing);
             this.ResumeLayout(false);
 
